35b3d91886eea310cf317d70074e19e2141c23b6
Mathias Parking <cph-mp525@cphbusiness.dk>
2022-05-08 12:30:56 +0200

    split

--- routes/redis.js
+++ routes/redis.js
@@ -22,6 +22,7 @@ router.get("/test", async function (req, res, next) {
   await client.set('test', '1');
   const value = await client.get('test');
   res.send("1 document inserted on test: " + value);
+  await client.quit();
 
 });
 
@@ -38,30 +39,39 @@ router.get("/datausa1", async function (req, res, next) {
 
     const {data} = await axios.get("https://datausa.io/api/data?measures=Average%20Wage,Average%20Wage%20Appx%20MOE&drilldowns=Detailed%20Occupation")
     await client.set("datausa1", JSON.stringify(data), {
-      EX: 10,
+      EX: 60,
     });
     
     res.json(data)
-    
+    await client.quit();
+
 });
 
 router.get("/datausa1/split", async function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   await client.connect();
-  /* client.get("datausa1", (error, datausa1)=>{
-    if(error) console.error(error);
-    if(datausa1 != null){
-      return res.json(JSON.parse(datausa1));
-    }
-  }); */
-
   const {data} = await axios.get("https://datausa.io/api/data?measures=Average%20Wage,Average%20Wage%20Appx%20MOE&drilldowns=Detailed%20Occupation")
-  /* await client.set("datausa1", JSON.stringify(data), {
-    EX: 10, GET DATA, GET OBJECT, MAKE LISTS/OTHER TYPES, INSERT IN REDIS
+  var list = data["data"]
+  
+  const retList = await client.lRange("datausa1list", 0, -1).catch((error)=>{
+    console.log(error);
   });
   
-  res.json(data) */
+  if(retList != null && retList.length > 0){
+    await client.quit();
+    return res.json(retList);
+  }
+
+   for (let i = 0; i < list.length; i++) {
+     await client.lPush("datausa1list", JSON.stringify(list[i]), {
+       EX:60,
+     });
+   }
+   
+  await client.quit();
+  
+  res.json("{\"status\":\"list stored\"}") 
   
 });
 
